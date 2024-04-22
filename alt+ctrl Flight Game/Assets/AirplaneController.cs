1bb0e41ae873666f4b1c8437b65f84fc196144eb
zPotatoMan <abp8171@utulsa.edu>
2024-04-22 15:20:22 -0500

    Added Pitch and Roll control

--- alt+ctrl Flight Game/Assets/AirplaneController.cs
+++ alt+ctrl Flight Game/Assets/AirplaneController.cs
@@ -7,30 +7,44 @@ public class AirplaneController : MonoBehaviour
 
     public float speed = 25.0f;
     public float turnSpeed = 1f;
+    public float rollSpeed = 1f;
+    public float pitchSpeed = 1f;
     public float horizontalInput;
     public float verticalInput;
+    public float rollInput;
+    public Rigidbody rb;
 
-    void Start()
+    private void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        rb.velocity = transform.forward * speed;
         bank();
+        altitude();
+        roll();
     }
 
     void bank () {
         horizontalInput = Input.GetAxis("Horizontal");
 
-        if (transform.rotation.z > -45 || transform.rotation.z < 45) {
-            transform.Rotate(0, 0, -turnSpeed * horizontalInput * Time.deltaTime);
-            transform.Rotate(0, turnSpeed * horizontalInput * Time.deltaTime, 0);
-        }
+        transform.Rotate(transform.forward * turnSpeed * horizontalInput * Time.deltaTime, Space.Self);
+        // transform.Rotate(0, turnSpeed * horizontalInput * Time.deltaTime, 0);
+
     }
 
-    void zero() { 
-        
+    void altitude() {
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Rotate(transform.right, pitchSpeed * verticalInput * Time.deltaTime, Space.Self);
+    }
+
+    void roll()
+    {
+        rollInput = Input.GetAxis("Roll");
+
+        transform.Rotate(0, 0, -rollSpeed * rollInput * Time.deltaTime);
     }
 }
