67487eb8398724e92cd13280b78cdd539e1b5edb
zPotatoMan <abp8171@utulsa.edu>
2024-04-24 15:19:34 -0500

    WHY IS THE SCENE REVERSED?!
    pain
    Messed around with rotation and banking.

--- alt+ctrl Flight Game/Assets/AirplaneController.cs
+++ alt+ctrl Flight Game/Assets/AirplaneController.cs
@@ -1,50 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AirplaneController : MonoBehaviour
 {
 
-    public float speed = 25.0f;
-    public float turnSpeed = 1f;
-    public float rollSpeed = 1f;
-    public float pitchSpeed = 1f;
-    public float horizontalInput;
-    public float verticalInput;
-    public float rollInput;
-    public Rigidbody rb;
+	public float speed = 25.0f;
+	public float turnSpeed = 1f;
+	public float rollSpeed = 1f;
+	public float pitchSpeed = 1f;
+	public float horizontalInput;
+	public float verticalInput;
+	public float rollInput;
+	public Rigidbody rb;
 
-    private void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	private void Start()
+	{
+		rb = GetComponent<Rigidbody>();
 
-    void Update()
-    {
-        rb.velocity = transform.forward * speed;
-        bank();
-        altitude();
-        roll();
-    }
+	}
 
-    void bank () {
-        horizontalInput = Input.GetAxis("Horizontal");
+	void Update()
+	{
+		horizontalInput = Input.GetAxis("Horizontal");
 
-        transform.Rotate(transform.forward * turnSpeed * horizontalInput * Time.deltaTime, Space.Self);
-        // transform.Rotate(0, turnSpeed * horizontalInput * Time.deltaTime, 0);
+		rb.velocity = transform.forward * speed;
 
-    }
+		if (horizontalInput != 0){
+			bank();
+		}
 
-    void altitude() {
-        verticalInput = Input.GetAxis("Vertical");
+		altitude();
+		roll();
+	}
 
-        transform.Rotate(transform.right, pitchSpeed * verticalInput * Time.deltaTime, Space.Self);
-    }
+	void bank () {		
+		transform.Rotate(new Vector3(0,0,1) * horizontalInput * Time.deltaTime * turnSpeed);
 
-    void roll()
-    {
-        rollInput = Input.GetAxis("Roll");
+		//if (horizontalInput > 0) {
+		//	rb.velocity += Vector3.right * turnSpeed;
+		//} else {
+		//	rb.velocity -= Vector3.right * turnSpeed;
+		//}
+	}
 
-        transform.Rotate(0, 0, -rollSpeed * rollInput * Time.deltaTime);
-    }
+	void altitude() {
+		verticalInput = Input.GetAxis("Vertical");
+
+		transform.Rotate(transform.right, pitchSpeed * verticalInput * Time.deltaTime);
+	}
+
+	void roll()
+	{
+		rollInput = Input.GetAxis("Roll");
+
+		transform.Rotate(0, 0, -rollSpeed * rollInput * Time.deltaTime);
+	}
 }
