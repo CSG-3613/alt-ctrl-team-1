775c4128deeceb62a020fef521ffb7aa307bf3e3
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2024-05-05 07:31:57 -0500

    controller edits
    - editing basic controller to include movement control
    - exported then fixed plane rotations and imported back in
    - set plane up as new character with scripts and settings
    - deleted old prefabs not going to be used

--- alt+ctrl Flight Game/Assets/Scripts/BasicController.cs
+++ alt+ctrl Flight Game/Assets/Scripts/BasicController.cs
@@ -6,9 +6,13 @@ using UnityEngine;
 
 public class BasicController : MonoBehaviour
 {
-    public float speed = 25.0f;
-    public float normalSpeed = 25.0f;
-    public float boostSpeed = 15.0f;
+    public float speed = 1.0f; //used to keep the state of the speed (normalSpeed or normalSpeed + boostSpeed)
+    public float normalSpeed = 1.0f; //the base speed the plane moves
+    public float boostSpeed = 3.0f; //the added speed the plane moves when on buttonDown
+
+    public float yawMultiplier; //to multiply the player input value (rotation AROUND y world axis)
+    public float pitchMultiplier; //to multiply the player input value (rotation AROUND x world axis)
+    public float rollMultiplier; //to multiply player input value (rotation AROUND z local axis)
 
     public Rigidbody rb;
 
@@ -22,20 +26,31 @@ public class BasicController : MonoBehaviour
         {
             Debug.Log(string.Format("Existing COM port: {0}", str));
         }
-        rb = GetComponent<Rigidbody>();
-        port.Open();
+        rb = GetComponent<Rigidbody>(); //getting the rigid body for the plane
+        port.Open(); //opening port on arduino for button
         Debug.Log("Port is Open");
     }
 
     // Update is called once per frame
     void Update()
     {
-        speed = SerialDataReading();
+        float yaw = Input.GetAxis("Vertical");
+        float pitch = Input.GetAxis("Horizontal");
 
-        rb.velocity = -transform.up * speed;
+        speed = SerialDataReading(); ///calls to readLine() input from arduino
+        rb.velocity = transform.forward * speed; //uses updated speed from SerialDatatReading() for rigid body velocity
+        rb.AddRelativeTorque(pitch * pitchMultiplier * Time.deltaTime, yaw * yawMultiplier * Time.deltaTime, -yaw * yawMultiplier * Time.deltaTime);
     }
 
-    public float SerialDataReading()
+    // FixedUpdate has 50 calls per second
+    void FixedUpdate() //called every 0.02 seconds keeping physics movement separate from frame rate
+    {
+        
+    }
+
+
+
+    public float SerialDataReading() // reads the input from the button and returns a float for the speed setting based on buttonDown or buttonUp
     {
         Debug.Log("entered SerialDataReading()");
         buttonString = port.ReadLine();
