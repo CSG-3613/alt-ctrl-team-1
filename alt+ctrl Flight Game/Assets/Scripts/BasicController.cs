42287c3b14d9e92f7d07e0872eebf4d6a85f67b0
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2024-05-05 09:11:45 -0500

    Updated BaiscController
    - updated basic controller using some of Ayden's controller script
    - added cube to hold wiimote demo script and added the cube to all spots in the array
    - added wiimote setup into basiccontroller script
    - editing for movement - NOT WORKING YET

--- alt+ctrl Flight Game/Assets/Scripts/BasicController.cs
+++ alt+ctrl Flight Game/Assets/Scripts/BasicController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.IO.Ports;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using WiimoteApi;
 
 public class BasicController : MonoBehaviour
 {
+    Wiimote gyro;
+
     public float speed = 1.0f; //used to keep the state of the speed (normalSpeed or normalSpeed + boostSpeed)
     public float normalSpeed = 1.0f; //the base speed the plane moves
     public float boostSpeed = 3.0f; //the added speed the plane moves when on buttonDown
+
+    float yawInput; //rotation AROUND y world axis
+    float pitchInput; //rotation AROUND x world axis
+    float rollInput; //rotation AROUND z local axis
 
     public float yawMultiplier; //to multiply the player input value (rotation AROUND y world axis)
     public float pitchMultiplier; //to multiply the player input value (rotation AROUND x world axis)
@@ -22,33 +29,85 @@ public class BasicController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        WiimoteManager.FindWiimotes();
+        gyro = WiimoteManager.Wiimotes[0];
+        gyro.SendDataReportMode(InputDataType.REPORT_BUTTONS_ACCEL_EXT16);
+        gyro.Accel.CalibrateAccel(AccelCalibrationStep.LEFT_SIDE_UP);
+        gyro.SendPlayerLED(false, false, false, true);
+
+        /*
         foreach (string str in SerialPort.GetPortNames()) //testing for ports on arduino uno
         {
             Debug.Log(string.Format("Existing COM port: {0}", str));
-        }
+        }*/
         rb = GetComponent<Rigidbody>(); //getting the rigid body for the plane
         port.Open(); //opening port on arduino for button
         Debug.Log("Port is Open");
+        StartCoroutine(activateWiimote());
     }
 
     // Update is called once per frame
     void Update()
     {
-        float yaw = Input.GetAxis("Vertical");
-        float pitch = Input.GetAxis("Horizontal");
+        if ( gyro.Button.home)
+        {
+            gyro.Accel.CalibrateAccel(AccelCalibrationStep.LEFT_SIDE_UP);
+        }
+        yawInput = Input.GetAxis("Vertical");
+        pitchInput = Input.GetAxis("Horizontal");
 
         speed = SerialDataReading(); ///calls to readLine() input from arduino
         rb.velocity = transform.forward * speed; //uses updated speed from SerialDatatReading() for rigid body velocity
-        rb.AddRelativeTorque(pitch * pitchMultiplier * Time.deltaTime, yaw * yawMultiplier * Time.deltaTime, -yaw * yawMultiplier * Time.deltaTime);
-    }
 
-    // FixedUpdate has 50 calls per second
-    void FixedUpdate() //called every 0.02 seconds keeping physics movement separate from frame rate
-    {
+        if (gyro != null)
+        {
+            Debug.Log("gyro is not null!");
+            float[] acell = gyro.Accel.GetCalibratedAccelData();
+            yawInput = acell[1];
+            //Debug.Log(yawInput.ToString());
+            bank();
+            pitchInput = acell[0];
+            //Debug.Log(pitchInput.ToString());
+            //pitch();
+            //rollInput = acell[0];
+            //roll();
+        }
         
     }
 
+    // FixedUpdate has 50 calls per second
+    IEnumerator activateWiimote()
+    {
+        yield return new WaitUntil(() => WiimoteManager.HasWiimote());
+    }
 
+    void bank() //incorporated from Ayden's script
+    {
+        transform.Rotate(Vector3.back * yawInput * Time.deltaTime * yawMultiplier);
+
+        if (yawInput > 0)
+        {
+            rb.velocity += Vector3.right / yawMultiplier;
+        }
+        else
+        {
+            rb.velocity -= Vector3.right / yawMultiplier;
+        }
+    }
+
+    void pitch() //incorporated from Ayden's script
+    {
+
+
+        transform.Rotate(Vector3.right * pitchMultiplier * pitchInput * Time.deltaTime);
+    }
+
+    void roll() //incorporated from Ayden's script
+    {
+        rollInput = Input.GetAxis("Roll");
+
+        transform.Rotate(0, 0, -rollMultiplier * rollInput * Time.deltaTime);
+    }
 
     public float SerialDataReading() // reads the input from the button and returns a float for the speed setting based on buttonDown or buttonUp
     {
