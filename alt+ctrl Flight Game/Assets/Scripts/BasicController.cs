df6dd744d4a03728dea56edff64f31aa4736fd0f
zPotatoMan <abp8171@utulsa.edu>
2024-05-06 12:37:17 -0500

    Edited BasicController.cs
    Cleaned up code formatting.
    Changed camera position and rotation to match plane's. Fixed the Parent Constraint on Main Camera.

--- alt+ctrl Flight Game/Assets/Scripts/BasicController.cs
+++ alt+ctrl Flight Game/Assets/Scripts/BasicController.cs
@@ -10,17 +10,19 @@ public class BasicController : MonoBehaviour
     Wiimote gyro;
     private bool recalibrateGyro = false;
 
-    public float speed = 1.0f; //used to keep the state of the speed (normalSpeed or normalSpeed + boostSpeed)
-    public float normalSpeed = 1.0f; //the base speed the plane moves
-    public float boostSpeed = 3.0f; //the added speed the plane moves when on buttonDown
+    public float speed = 1.0f;                                                 //used to keep the state of the speed (normalSpeed or normalSpeed + boostSpeed)
+    public float normalSpeed = 1.0f;                                           //the base speed the plane moves
+    public float boostSpeed = 3.0f;                                            //the added speed the plane moves when on buttonDown
 
-    float yawInput; //rotation AROUND y world axis
-    float pitchInput; //rotation AROUND x world axis
-    float rollInput; //rotation AROUND z local axis
+    float yawInput;                                                            //rotation AROUND y world axis
+    float pitchInput;                                                          //rotation AROUND x world axis
+    float rollInput;                                                           //rotation AROUND z local axis
 
-    public float yawMultiplier; //to multiply the player input value (rotation AROUND y world axis)
-    public float pitchMultiplier; //to multiply the player input value (rotation AROUND x world axis)
-    public float rollMultiplier; //to multiply player input value (rotation AROUND z local axis)
+    public float yawMultiplier;                                                //to multiply the player input value (rotation AROUND y world axis)
+    public float pitchMultiplier;                                              //to multiply the player input value (rotation AROUND x world axis)
+    public float rollMultiplier;                                               //to multiply player input value (rotation AROUND z local axis)
+    
+    public float buffer = 0.5f;                                                //buffer to prevent unintentional inputs
 
     public Rigidbody rb;
 
@@ -43,8 +45,8 @@ public class BasicController : MonoBehaviour
         {
             Debug.Log(string.Format("Existing COM port: {0}", str));
         }*/
-        rb = GetComponent<Rigidbody>(); //getting the rigid body for the plane
-        port.Open(); //opening port on arduino for button
+        rb = GetComponent<Rigidbody>();                                         //getting the rigid body for the plane
+        port.Open();                                                            //opening port on arduino for button
         Debug.Log("Port is Open");
         StartCoroutine(activateWiimote());
     }
@@ -52,7 +54,7 @@ public class BasicController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if ( gyro.Button.a)
+        if (gyro.Button.a)
         {
             gyro.Accel.CalibrateAccel(AccelCalibrationStep.LEFT_SIDE_UP);
             Debug.Log("reset calibration");
@@ -60,32 +62,35 @@ public class BasicController : MonoBehaviour
         //yawInput = Input.GetAxis("Vertical"); //not needed with wiimote
         //pitchInput = Input.GetAxis("Horizontal"); // not needed with wiimote
         
-        speed = SerialDataReading(); ///calls to readLine() input from arduino
+        speed = SerialDataReading();                                            //calls to readLine() input from arduino
 
-        rb.velocity = transform.forward * speed; //uses updated speed from SerialDatatReading() for rigid body velocity
+        rb.velocity = transform.forward * speed;                                //uses updated speed from SerialDataReading() for rigid body velocity
 
         if (gyro != null)
         {
             Debug.Log("gyro is not null!");
             float[] acell = gyro.Accel.GetCalibratedAccelData();
-            yawInput = -acell[1] + 0.3f;
+
+            yawInput = -acell[1] + buffer;
             //Debug.Log(yawInput.ToString());
             bank();
-            pitchInput = acell[0] - 0.3f;
+
+            pitchInput = acell[0] - buffer;
             //Debug.Log(pitchInput.ToString());
             pitch();
-            rollInput = (acell[1] + 0.3f);
+
+            rollInput = acell[1] + buffer;
             //roll();
 
-            if(pitchInput > -0.3f &&  pitchInput < 0.3f) pitchInput = 0; //removes unintentional rotation
-            if (yawInput > -0.3f && yawInput < 0.3f) yawInput = 0; //removes unintentional rotation
+            if (pitchInput > -buffer &&  pitchInput < buffer) pitchInput = 0;   //removes unintentional rotation
+            if (yawInput > -buffer && yawInput < buffer) yawInput = 0;          //removes unintentional rotation
         }
         
     }
 
     IEnumerator activateWiimote()
     {
-        yield return new WaitUntil(() => WiimoteManager.HasWiimote()); //delays everything until the wiimotes are found
+        yield return new WaitUntil(() => WiimoteManager.HasWiimote());          //delays everything until the wiimotes are found
     }
 
     void bank() //incorporated from Ayden's script
@@ -93,22 +98,19 @@ public class BasicController : MonoBehaviour
         transform.Rotate(0, yawInput * yawMultiplier * Time.deltaTime, 0);
         //transform.Rotate(Vector3.up * yawInput * Time.deltaTime * yawMultiplier); //changed from back to up to rotate around the y axis for better turning control
 
-        //transform.Rotate(Vector3.back * yawInput * Time.deltaTime * yawMultiplier);
-
         if (yawInput > 0)
         {
-            rb.velocity += Vector3.right / yawMultiplier;
+            rb.velocity += Vector3.right * yawMultiplier;
+            transform.Rotate(0, 0, yawInput * yawMultiplier * Time.deltaTime);
         }
         else
         {
-            rb.velocity -= Vector3.right / yawMultiplier;
+           rb.velocity -= Vector3.right * yawMultiplier;
+           transform.Rotate(0, 0, -yawInput * yawMultiplier * Time.deltaTime);
         }
     }
 
-    void pitch() //incorporated from Ayden's script
-    {
-
-
+    void pitch() { //incorporated from Ayden's script
         transform.Rotate(Vector3.right * pitchMultiplier * pitchInput * Time.deltaTime);
     }
 
@@ -135,8 +137,7 @@ public class BasicController : MonoBehaviour
         Debug.Log("entered SerialDataReading()");
         buttonString = port.ReadLine();
        
-        if (buttonString == "buttonDown")
-        {
+        if (buttonString == "buttonDown") {
         Debug.Log("entered button if");
             return speed = normalSpeed + boostSpeed;
         }
