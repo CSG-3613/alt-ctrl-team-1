e32ad35c09f2a8548c69c98584980b3b7031d420
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2024-05-06 17:40:24 -0500

    Update CameraFollow.cs
    -added variables for rotation in camerfollow script and and for a lookat target (this is not quite working right but is getting close)

--- alt+ctrl Flight Game/Assets/Scripts/Ayden/CameraFollow.cs
+++ alt+ctrl Flight Game/Assets/Scripts/Ayden/CameraFollow.cs
@@ -1,3 +1,4 @@
+/* Script edited by Bridget Kurr May 2024 */
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,19 +7,35 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private GameObject target;
+    [SerializeField] private Transform targetT;
     [SerializeField] private float speed;
     [SerializeField] private float threshold;
     [SerializeField] private Vector3 offset;
+    
+
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 target_position = target.transform.position + offset;
-        Vector3 cam_position = transform.position;
+        Debug.Log("Entered Camera Update Method");
+        Vector3 target_position = target.transform.position ;
+        Debug.Log("target's position: " + target_position);
+        Vector3 cam_position = target_position + offset;
+        Debug.Log("camera position values: " + cam_position);
+
+        Quaternion target_rotation = target.transform.rotation;
+        Debug.Log("target's rotation: " + target_rotation);
+        Quaternion cam_rotation = target_rotation;
+        cam_rotation.z = 0;
+        Debug.Log("camera rotation values: " + cam_rotation);
+
+        transform.position = cam_position;
+        transform.rotation = cam_rotation;
+        transform.LookAt(targetT);
 
         // Get a vector in direction of target
-        Vector3 direction = target_position - cam_position;
-        direction.y = 0;
+        /*Vector3 direction = target_position - cam_position;
+        //direction.y = 0;
 
         // Only move if the movement is significant enough
         if (direction.magnitude > threshold)
@@ -27,7 +44,8 @@ public class CameraFollow : MonoBehaviour
 
             cam_position += direction * speed * Time.deltaTime;
             transform.position = cam_position;
-        }
+            transform.rotation = cam_rotation;
+        }*/
     }
 }
 
