c8741f52720ebbeab34825d93551422fbc72efaa
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2024-05-06 17:42:38 -0500

    Update CameraFollow.cs
    -swapped the order of execution for look at and changing position rotation and that fixed the 2d quality of the follow but it is still not right

--- alt+ctrl Flight Game/Assets/Scripts/Ayden/CameraFollow.cs
+++ alt+ctrl Flight Game/Assets/Scripts/Ayden/CameraFollow.cs
@@ -29,9 +29,10 @@ public class CameraFollow : MonoBehaviour
         cam_rotation.z = 0;
         Debug.Log("camera rotation values: " + cam_rotation);
 
+        transform.LookAt(targetT);
         transform.position = cam_position;
         transform.rotation = cam_rotation;
-        transform.LookAt(targetT);
+        
 
         // Get a vector in direction of target
         /*Vector3 direction = target_position - cam_position;
